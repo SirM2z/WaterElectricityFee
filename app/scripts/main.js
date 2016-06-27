bac921c2df93e88babe3042cd4626ddc3f225713
z <SirM2z@users.noreply.github.com>
2016-06-27 13:34:38 +0800

    delet ip
    delet ip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,6 @@
 /*global app, $*/
 
-//var APIServerHost= 'http://120.26.48.150:8089/';
-var APIServerHost= 'http://pay.houqinbao.com:8089/';
-//var APIServerHost ='http://localhost:9289/';
-//var APIServerHost= 'http://121.41.82.81:8090/';
+var APIServerHost= 'http://IP:port/';
 
 window.App = {
   Models: {},
